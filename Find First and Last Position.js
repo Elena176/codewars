ad50e77b32b98f77711a5a8fee108d494b614bf6
Elena176 <lena.tdn176@gmail.com>
2022-09-18 00:14:20 +0300

    added Find First and Last Position of Element

---
+++ Find First and Last Position.js
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
+
+const searchRange = function(nums, target) {
+    const first = nums.indexOf(target)
+    const last = nums.lastIndexOf(target)
+    return [first, last]
+};
---
+++ MergeTwoSortedLists.js
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/merge-two-sorted-lists/
+*/
+/*let mergeTwoLists = function(list1, list2) {
+  return list1.concat(list2).sort((a, b) => a - b);
+};
+mergeTwoLists([1,2,4], [1,3,4]);*/
+/*
+const majorityElement = function (nums) {
+    return Number(Object.entries(nums.reduce((acc, el) => {
+        acc[el] = acc[el] ? acc[el] + 1 : 1
+        return acc;
+    }, {})).sort((a, b) => b[1] - a[1])[0][0]);
+};
+majorityElement([2, 2, 1, 1, 1, 2, 2])
+*/
